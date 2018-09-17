b9d4a7546fb38f6b94719b5b28d4f547b7746b33
xKxAxKx <pekepooooong@gmail.com>
2018-09-18 08:56:11 +0900

    update:add componets:

---
+++ src/components/Editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="editor">
+    <h1>エディター画面</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'editor',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+</style>
+    
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button>Google アカウントでログイン </button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {}
+  }
+}
+msg: 'Welcome to MyMarkdown'
+</script>
