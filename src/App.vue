d0fd809d631b65fcd0abb8e78cb82b91829ba324
xKxAxKx <pekepooooong@gmail.com>
2018-09-24 17:55:58 +0900

    update:App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Home v-if="!login"></Home>
-    <Editor v-if="login"></Editor>
+    <Editor v-if="login" :user="userData"></Editor>
   </div>
 </template>
 <script>
@@ -14,19 +14,21 @@ export default{
       login: false
     }
   },
-  components: {
-    'Home': Home,
-    'Editor': Editor,
-  },
   created: function() {
     firebase.auth().onAuthStateChanged(user => {
       console.log(user);
       if(user) {
         this.isLogin = true;
+        this.userdata = user;
       } else {
         this.isLogin = false;
+        this.userdata = null;
       }
     })
-  }
+  },
+  components: {
+    'Home': Home,
+    'Editor': Editor,
+  },
 }
 </script>
