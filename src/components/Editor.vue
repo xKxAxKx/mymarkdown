4d32146c95070cbc50cb289719d90bcb1fe936bb
xKxAxKx <pekepooooong@gmail.com>
2018-09-24 19:37:16 +0900

    update:add marked

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,21 +2,42 @@
   <div class="editor">
     <h1>エディター画面</h1>
     <button @click="logout"> ログアウト </button>
+    <div​ class="editorWrapper">​
+      <textarea​ class="markdown" v-model="markdown"></textarea>​
+      <div class="preview" ​v-html="preview()"></div>
+    </div​>
   </div>
 </template>
 <script>
+import marked from 'marked';
 export default {
   name: 'editor',
   props: ['user'],
   data () {
-    return {}
+    return {
+      markedown: '',
+    }
   },
   methods: {
     logout: function() {
       firebase.auth().signOut();
+    },
+    preview: function() {
+      return marked(this.markdown);
     }
   }
 }
 </script>
 <style lang="scss">
+.editorWrapper {
+  display: flex;
+}
+.markdown {
+  width: 50%;
+  height: 500px;
+}
+.preview {
+  width: 50%;
+  text-align: left;
+}
 </style>
