e8e12e2f4108d0292a63151905b5576c006655f6
xKxAxKx <pekepooooong@gmail.com>
2018-09-24 18:16:33 +0900

    update:Editor.vue

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="editor">
     <h1>エディター画面</h1>
+    <span>{{ user.displayName }}</span>
+    <button @click="logout"> ログアウト </button>
   </div>
 </template>
 <script>
 export default {
   name: 'editor',
+  props: ['user'],
   data () {
     return {}
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut();
+    }
   }
 }
 </script>
 <style lang="scss">
 </style>
-    
