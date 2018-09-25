7f70c8261f3a3e5c386cb24b0fe9216597298a72
xKxAxKx <pekepooooong@gmail.com>
2018-09-25 09:19:32 +0900

    update:Editor.vue

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,10 +2,16 @@
   <div class="editor">
     <h1>エディター画面</h1>
     <button @click="logout"> ログアウト </button>
-    <div​ class="editorWrapper">​
-      <textarea​ class="markdown" v-model="markdown"></textarea>​
-      <div class="preview" ​v-html="preview()"></div>
-    </div​>
+    <div>
+      <div class="memoListWrapper">
+        <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" data-selected="index==selectedIndex">
+          <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+        </div>
+        <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+      </div>
+    </div>
+    <textarea​ class="markdown" v-model="memos[selectedIndex].markdown"></textarea>​
+    <div class="preview" ​v-html="preview()"></div>
   </div>
 </template>
 <script>
@@ -15,29 +21,70 @@ export default {
   props: ['user'],
   data () {
     return {
-      markedown: '',
+      memos: [{
+        markdown: ''
+      }],
     }
   },
   methods: {
     logout: function() {
       firebase.auth().signOut();
     },
+    addMemo: function() {
+      this.memo.push({
+        markdown: '無題のメモ',
+      })
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
     preview: function() {
-      return marked(this.markdown);
-    }
+      return marked(this.memos.[this.selectedIndex].markdown);
+    },
+    displayTitle: function(text) {
+      return text.split(/\n/)[0];​
+    },
   }
 }
 </script>
 <style lang="scss">
+.memoListWrapper {
+  width: 19%;
+  float: left;
+  border-top: 1px solid #000;
+}
+.memoList {
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: left;
+  border-bottom: 1px solid #000;
+  &:nth-child(even) {
+    background-color: #ccc;
+  }
+  &[data-selected="true"] {
+    background-color: #ccf;
+  }
+}
+.memoTitle {
+  height: 1.5em;
+  margin: 0;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.addMemoBtn {
+  margin-top: 20px;
+}
 .editorWrapper {
   display: flex;
 }
 .markdown {
-  width: 50%;
+  float: left;
+  width: 40%;
   height: 500px;
 }
 .preview {
-  width: 50%;
+  float: left;
+  width: 40%;
   text-align: left;
 }
 </style>
